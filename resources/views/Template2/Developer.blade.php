6f628799ff3ff68d69b25d3b766aa3a9f17a8801
lgarin211 <lagustinus211@gmail.com>
2021-01-18 22:19:42 +0700

    malam

---
+++ resources/views/Template2/Developer.blade.php
@@ -0,0 +1,61 @@
+<section>
+    <div class="container">
+        <div class="container marketing">
+
+
+            <!-- START THE FEATURETTES -->
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">M Rizky <br> <span class="text-muted">Forn-End
+                            Developer</span></h2>
+                    <p class="lead"><a href="detail.html">
+                            see detail
+                        </a></p>
+                </div>
+                <div class="col-md-5">
+                    <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500px" height="500px" src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Agustinus Pardamean Lumban Tobing<br> <span class="text-muted">FullStak-Developert</span></h2>
+                    <p class="lead"><a href="detail.html">
+                            see detail
+                        </a></p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500px" height="500px" src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Nur salam<br> <span class="text-muted">Documentasi</span>
+                    </h2>
+                    <p class="lead"><a href="detail.html">
+                            see detail
+                        </a></p>
+                </div>
+                <div class="col-md-5">
+                    <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500px" height="500px" src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- /END THE FEATURETTES -->
+
+        </div>
+    </div>
+</section>
---
+++ resources/views/Template2/about.blade.php
@@ -0,0 +1,16 @@
+<!-- Two -->
+<section id="two">
+    <div class="inner">
+        <header class="major">
+            <h2>abaout</h2>
+        </header>
+        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna
+            magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna.
+            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem
+            consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
+            amet pharetra et feugiat tempus.</p>
+        <ul class="actions">
+            <li><a href="landing.html" class="button next">Get Started</a></li>
+        </ul>
+    </div>
+</section>
