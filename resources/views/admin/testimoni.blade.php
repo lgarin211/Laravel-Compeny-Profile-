24ff847b43abb3703b6fa3bdbe2f6fbfb2d4cbfc
lgarin211 <lagustinus211@gmail.com>
2021-01-18 16:54:02 +0700

    agus

--- resources/views/admin/testimoni.blade.php
+++ resources/views/admin/testimoni.blade.php
@@ -35,17 +35,17 @@
                     <td>{{$item->Deskripsi}}</td>
                     <td>
                         <a href="{{$item->Gambar}}">
-                            <img src="{{$item->Gambar}}" class="w-50" style="max-height: 200px; width: fit-content" >
-
+                            <img src="{{$item->Gambar}}" class="w-50" style="max-height: 200px; width: fit-content">
                         </a>
                     </td>
                     <td>
                         <div class="row">
-                            <button type="button" class="btn btn-danger col-md-6">
-                                <i class="fas fa-trash" aria-hidden="true"></i>
-                            </button>
-                            <button type="button" class="btn btn-success col-md-6">
+                            <a href="pas?tabel=testimonies&id={{$item->id}}&view=edittestimoni" type="button"
+                                class="btn btn-danger col-md-6">
                                 <i class="fas fa-user-edit" aria-hidden="true"></i>
+                            </a>
+                            <button type="button" class="btn btn-success col-md-6">
+                                <i class="fas fa-trash" aria-hidden="true"></i>
                             </button>
                         </div>
                     </td>
