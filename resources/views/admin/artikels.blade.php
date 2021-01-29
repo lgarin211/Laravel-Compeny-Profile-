20a880afccbd02712413f6563134f6e76a520cb8
lgarin211 <lagustinus211@gmail.com>
2021-01-29 15:54:16 +0700

    as

--- resources/views/admin/artikels.blade.php
+++ resources/views/admin/artikels.blade.php
@@ -8,18 +8,23 @@
 @endif
 <form method="POST" action="add">
     <input type="hidden" name="tabel" value="artikels">
-    @forelse ($data['artikels'] as $key=>$item)
     @csrf
-    @if ($key=='pas')
+    @foreach ($data['artikels'] as $key=>$item)
+    @if ($key==1)
+    @else
+    @foreach ($item as $key2=>$value)
+    @if ($key2=='ce')
     @include('admin.ck')
-    @endif
+    @else
     <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">{{ $key }}</span>
-        <input type="text" class="form-control" placeholder="{{ $key }}" aria-label="{{ $key }}" aria-describedby="basic-addon1">
+        <span class="input-group-text" id="basic-addon1">{{ $key2 }}</span>
+        <input type="text" class="form-control" name="{{ $key2 }}" placeholder="{{ $value }}" aria-describedby="basic-addon1">
     </div>
-    @empty
-    {{-- empty expr --}}
-    @endforelse
+    @endif
+    @endforeach
+    @endif
+    @endforeach
+
     <button type="submit" class="btn btn-primary">kirim artikel</button>
 </form>
 
