90a413a0f9952dd2701aa71601fa6cac8af34e65
lgarin211 <lagustinus211@gmail.com>
2021-01-30 22:03:09 +0700

    pass

--- resources/views/Template3/foot.blade.php
+++ resources/views/Template3/foot.blade.php
@@ -98,7 +98,9 @@
 @endforeach
 @include('Template3.modal')
 <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
-<div id="preloader"></div>
+
+{{-- loading here--}}
+{{-- <div id="preloader"></div> --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
     integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
     crossorigin="anonymous"></script>
@@ -130,28 +132,7 @@
             }
         })
 </script>
-<script type="text/javascript">
-    $('.noknok').owlCarousel({
-            loop:true,
-            center: true,
-            items:4,
-            autoplay:true,
-            autoplayTimeout:5000,
-            autoplayHoverPause:true,
-            // margin:10,
-            // autoWidth:true,
-            // nav:true,
-            responsive:{
-                0:{
-                    items:1
-                },
-                // 100:{
-                //     items:2
-                // },
 
-            }
-        })
-</script>
 
 <!-- Vendor JS Files -->
 <script src="{{asset('assetsArsha')}}/vendor/jquery/jquery.min.js"></script>
